f41d5d19cc196cc5ff2973816746fab19a6b22b8
Haruka Kataoka <hrk8@drumsoft.com>
2020-11-06 15:30:41 +0900

    created by "pod lib create TccSwift"

---
+++ Example/Pods/Target Support Files/Pods-TccSwift_Tests/Pods-TccSwift_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TccSwift_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TccSwift_Tests
+@end
---
+++ Example/Pods/Target Support Files/TccSwift/TccSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TccSwift : NSObject
+@end
+@implementation PodsDummy_TccSwift
+@end
