e9bb9e8835495dd3c53e4deb59225b7da172549a
Haruka Kataoka <hrk8@drumsoft.com>
2020-11-10 03:00:31 +0900

    Id, Battery, Button, Sound feature sample in Example

--- Example/TccSwift/CubeControllerPage.swift
+++ Example/TccSwift/CubeControllerPage.swift
@@ -22,11 +22,93 @@ class CubeControllerPage: UIViewController, CubeDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         cube.delegate = self
+        _ = cube.startNotifyId() { self.idNotified($0) }
+        _ = cube.startNotifyButton() { self.buttonNotified($0) }
+        _ = cube.startNotifyBattery() { self.batteryNotified($0) }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         cube.disconnect()
+    }
+    
+    // MARK: notification
+    
+    private var currentPosition:IdResponse?
+    
+    private func idNotified(_ result:Result<IdResponse, Error>) {
+        switch result {
+        case .success(let idResponse):
+            self.currentPosition = idResponse
+            self.statusUpdated()
+        case .failure(let error):
+            self.alertError(error)
+        }
+    }
+    
+    private var soundEffectIndex:Int = 0
+    private func playSound() {
+        var se = SoundEffect.init(rawValue: soundEffectIndex)
+        if se == nil {
+            se = SoundEffect.init(rawValue: 0)
+            soundEffectIndex = 0
+        }
+        cube.writeSoundPlay(se: se!, volume: 1) {
+            switch $0 {
+            case .success(_):
+                print("writeSoundPlay succeeded")
+            case .failure(let error):
+                self.alertError(error)
+            }
+        }
+        soundEffectIndex += 1
+    }
+    
+    private func buttonNotified(_ result:Result<ButtonResponse, Error>) {
+        switch result {
+        case .success(let response):
+            switch response {
+            case let b as ButtonFunctionResponse:
+                if b.isPushed {
+                    self.playSound()
+                }
+            default:
+                break
+            }
+        case .failure(let error):
+            self.alertError(error)
+        }
+    }
+    
+    private var battery:Int?
+    
+    private func batteryNotified(_ result:Result<BatteryResponse, Error>) {
+        switch result {
+        case .success(let batteryResponse):
+            self.battery = batteryResponse.capacity
+            self.statusUpdated()
+        case .failure(let error):
+            self.alertError(error)
+        }
+    }
+    
+    @IBOutlet weak var labelStatus: UILabel!
+    
+    private func statusUpdated() {
+        let positionText:String
+        switch currentPosition {
+        case let p as IdPositionResponse:
+            positionText = "Position ID: (\(p.cubeX), \(p.cubeY)), rotation: \(p.cubeRotation)"
+        case let p as IdStandardResponse:
+            positionText = "Standard ID: \(p.id), rotation: \(p.cubeRotation)"
+        case is IdPositionIdMissedResponse:
+            positionText = "removed from Position ID"
+        case is IdStandardResponse:
+            positionText = "removed from Standard ID"
+        default:
+            positionText = "no ID provided."
+        }
+        labelStatus.text = "battery: \(battery.flatMap{String($0)} ?? "?") %\n\(positionText)"
     }
 
     // MARK: sliders for motor
@@ -53,7 +135,6 @@ class CubeControllerPage: UIViewController, CubeDelegate {
         let left = Int(round(sliderLeft.value * 115))
         let right = Int(round(sliderRight.value * 115))
         if current_left != left || current_right != right {
-            print("MOTOR: \(left), \(right)")
             cube.writeActivateMotor(left: left, right: right)
             current_left = left
             current_right = right
@@ -63,6 +144,12 @@ class CubeControllerPage: UIViewController, CubeDelegate {
     // MARK: CubeDelegate
     
     func cube(_ cube: Cube, didReceivedUnhandled error: Error) {
+        self.alertError(error)
+    }
+    
+    // MARK: Alert
+    
+    func alertError(_ error:Error) {
         let alertController = UIAlertController(title: "Error from Cube", message: error.localizedDescription, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: .default))
         self.present(alertController, animated: true, completion: nil)
